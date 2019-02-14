1944aa9697046970252954b7f1959d5726a2cde6
ravikant singh <ravi1984aug@gmail.com>
2019-02-13 21:31:51 -0500

    initial spring boot app

---
+++ src/main/java/org/cognizant/projectmanagement/Application.java
@@ -0,0 +1,18 @@
+package org.cognizant.projectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = "org.cognizant")
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
